6602425148e48df1c6e3975769367cf29e59ac82
IamRabin <rabindra1.khadka1@gmail.com>
2023-11-17 11:05:38 +0100

    bugs fixing

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 
 import os
 import inspect
+import sphinx_rtd_theme
 from sphinx.ext import apidoc
 
 
@@ -201,4 +202,6 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
